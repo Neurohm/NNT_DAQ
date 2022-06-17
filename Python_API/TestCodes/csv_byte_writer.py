e124baf6e86713d7efec63a9b5930e54b05d3240
omkolhe <omkolhe026@gmail.com>
2022-06-17 15:24:31 -0400

    Finished testing write_to_csv function for write bytearray to a row of csv file

--- Python_API/TestCodes/csv_byte_writer.py
+++ Python_API/TestCodes/csv_byte_writer.py
@@ -5,20 +5,34 @@ import numpy as np
 import matplotlib as plt
 import random 
 
+
+def write_to_csv(writer, buff):
+    data_row = [None]*16
+    for i in range(0,16):
+        for j in range(0,16):
+            sample_byte =  buff[(i*2*16 + j*2):(i*2*16 + j*2 + 2)]
+            data_row[j] = int.from_bytes(sample_byte, "big", signed = "False")
+        writer.writerow(data_row)
+    
+
+
 def randbytes(n):
     for _ in range(n):
         yield random.getrandbits(8)
 
-def print_help():
-    print("The program will generate a byte array and write it to a cs file the user provides.")
-    print("csv_byte_writer.py syntax: \n python3 csv_byte_writer.py path_to_csv_file")
-
 def main():
     print("Generating 512 bytes byte array")
-    buff = bytearray(randbytes(512))
-    print(len(buff))
+    buff1 = bytearray(randbytes(512))
+    buff2 = bytearray(randbytes(512))
+    
 
+    # Opening the csv file for write mode 
+    f = open(sys.argv[1],'w',newline='')
+    writer = csv.writer(f)
+    write_to_csv(writer,buff1)
+    write_to_csv(writer,buff2)
 
+    f.close()
 
 if __name__ == "__main__":
     main()
