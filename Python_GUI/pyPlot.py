c8e158d9847862afcebffd43401a0f3d6f0cfaee
omkolhe <omkolhe026@gmail.com>
2022-08-12 20:21:36 -0400

    Live updating graph using pygraph. WIP: Using QTDesigner to desing the GUI.

--- Python_GUI/pyPlot.py
+++ Python_GUI/pyPlot.py
@@ -1,8 +1,11 @@
-from PyQt5 import QtWidgets
+# https://www.pythonguis.com/tutorials/plotting-pyqtgraph/
+
+from PyQt5 import QtWidgets, QtCore
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import os
+from random import randint
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -12,19 +15,54 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
 
-        hour = [1,2,3,4,5,6,7,8,9,10]
-        temperature = [30,32,34,32,33,31,29,32,35,45]
+        self.x = list(range(100))  # 100 time points
+        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
 
-        # plot data: x, y values
-        self.graphWidget.plot(hour, temperature)
+        #Add Background colour to white
+        self.graphWidget.setBackground('w')
+        # Add Title
+        self.graphWidget.setTitle("Your Title Here", color="b", size="30pt")
+        # Add Axis Labels
+        styles = {"color": "#f00", "font-size": "20px"}
+        self.graphWidget.setLabel("left", "Temperature (°C)", **styles)
+        self.graphWidget.setLabel("bottom", "Hour (H)", **styles)
+        #Add legend
+        self.graphWidget.addLegend()
+        #Add grid
+        self.graphWidget.showGrid(x=True, y=True)
+        #Set Range
+        # self.graphWidget.setXRange(0, 10, padding=0.05)
+        # self.graphWidget.setYRange(20, 55, padding=0.05)
 
+        # Remove the lines but keep all the other attributes same
+        # self.graphWidget.clear()
+       
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def plot(self, x, y, plotname, color):
+        pen = pg.mkPen(color=color)
+        self.graphWidget.plot(x, y, name=plotname,  pen=pen, symbol='.', symbolSize=20, symbolBrush=('b'))
+        
+    def update_plot_data(self):
+        self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+        self.y = self.y[1:]  # Remove the first
+        self.y.append( randint(0,100))  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)  # Update the data.
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
     main.show()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
